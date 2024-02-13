90322d96e956940383f90d9dd2a60a578112a8cd
Egorov Andrey <134514892+InfernalBeard@users.noreply.github.com>
2024-02-13 19:32:46 +0300

    Create Script-3.sql

---
+++ Script-3.sql
@@ -0,0 +1,7 @@
+/*
+3. Напишите скрипт, который будет возвращать из таблиц поля `product_name`
+для пользователей с именем `alexey` независимо от регистра ввода имени.
+*/
+SELECT product_name FROM netology.ORDERS
+INNER JOIN netology.CUSTOMERS c on c.id = orders.customer_id
+WHERE lower(name) = 'alexey'
